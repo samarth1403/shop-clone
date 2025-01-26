fd8983ee7c2813b91efc5c35d7bb978e227afad5
Samarth Parmeshwar Ikkalaki <samarthikkalaki123@gmail.com>
2025-01-26 05:48:29 +0530

    Removed React Import from Provider

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,5 +1,5 @@
+import { ReactNode } from "react";
 import { GlobalProvider } from "./GlobalProvider";
-import React, { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
   return <GlobalProvider>{children}</GlobalProvider>;
